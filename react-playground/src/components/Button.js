b2c6b0bf153bc990a4b1bba863322404ee7d1ffd
Sum Tsui <sumtsui@gmail.com>
2020-12-08 22:48:10 +0800

    react

--- react-playground/src/components/Button.js
+++ react-playground/src/components/Button.js
@@ -1,13 +1,20 @@
-import {useState} from 'react'
+import { useState } from 'react';
 
 function Button() {
-  const [bgColor, setBgColor] = useState('yellow')
+  const [bgColor, setBgColor] = useState('yellow');
+
+  console.log('Button');
 
   return (
-    <button onClick={() => setBgColor(prev => prev === 'yellow' ? 'orange' : 'yellow')} style={{backgroundColor: bgColor}}>
+    <button
+      onClick={() =>
+        setBgColor((prev) => (prev === 'yellow' ? 'orange' : 'yellow'))
+      }
+      style={{ backgroundColor: bgColor }}
+    >
       Click
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
--- react-playground/src/components/Header.js
+++ react-playground/src/components/Header.js
@@ -1,9 +1,11 @@
 function Header(props) {
+  console.log('Header');
+
   return (
     <header>
       <h3>The header</h3>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
