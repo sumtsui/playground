080540f2a1043c946c95bef28cbe98f4ab8c8ef7
Sum Xu <v-suxu@expedia.com>
2020-12-31 16:43:08 +0800

    update

--- node-stream/example1.js
+++ node-stream/example1.js
@@ -1,8 +1,8 @@
-var Readable = require("stream").Readable;
+var Readable = require('stream').Readable;
 
 var rs = new Readable();
-rs.push("beep ");
-rs.push("boop\n");
+rs.push('beep ');
+rs.push('boop\n');
 rs.push(null);
 
 rs.pipe(process.stdout);
