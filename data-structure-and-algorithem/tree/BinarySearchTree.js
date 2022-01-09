b7714bc28c1505dc97eb538b61103ff8fb25a136
Sum Tsui <sumtsui@gmail.com>
2022-01-09 22:27:58 +0800

    tree traversal

---
+++ data-structure-and-algorithem/tree/BinarySearchTree.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+  
+  insert(value){
+    var newNode = new Node(value);
+    if (this.root === null){
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (current){
+      if (value === current.value) return undefined;
+      if (value < current.value){
+        if (current.left === null){
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null){
+          current.right = newNode;
+          return this;
+        } 
+        current = current.right;
+      }
+    }
+  }
+}
+
+module.exports = BinarySearchTree;
--- data-structure-and-algorithem/tree/depth-first-search.js
+++ data-structure-and-algorithem/tree/depth-first-search.js
@@ -1,18 +1,46 @@
+const BinarySearchTree = require('./BinarySearchTree');
+
+const Tree = new BinarySearchTree();
+Tree.insert(19);
+Tree.insert(30);
+Tree.insert(3);
+Tree.insert(5);
+Tree.insert(17);
+Tree.insert(8);
+Tree.insert(24);
+
+// console.log('tree', Tree);
+
 // DFS
 /**
- *       10
- *     2     6 
- *   5  12  8
- */
+ *           19
+ *     3          30      
+ *       5      24       
+ *         17    
+ *        8
+*/
+
 // preorder
 // recursive approach
-function DFS(node) {
-  const data = [];
-  function traverse(cur) {
-    data.push(cur.value);
-    if (cur.left) traverse(cur.left);
-    if (cur.right) traverse(cur.right); 
+function preorder(node) {
+  if (node) {
+    console.log(node.value);
+    preorder(node.left);
+    preorder(node.right);
   }
-  traverse(node);
-  return data;
 }
+function inorder(node) {       
+  node.left && inorder(node.left); //step 1
+  console.log(node.value);
+  node.right && inorder(node.right); //step 3
+}
+// function inorder(node) {
+//   if (node) {
+//     if (node.left) preorder(node.left);
+//     console.log(node.value);
+//     if (node.right) preorder(node.right);
+//   } 
+// }
+
+// preorder(Tree.root);
+inorder(Tree.root);
