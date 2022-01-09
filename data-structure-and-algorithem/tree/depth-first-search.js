21612ee05099aad0b1d8a7e4b91b625e825e0e92
Sum Tsui <sumtsui@gmail.com>
2022-01-09 23:07:19 +0800

    update

--- data-structure-and-algorithem/tree/depth-first-search.js
+++ data-structure-and-algorithem/tree/depth-first-search.js
@@ -1,6 +1,7 @@
 const BinarySearchTree = require('./BinarySearchTree');
 
 const Tree = new BinarySearchTree();
+
 Tree.insert(19);
 Tree.insert(30);
 Tree.insert(3);
@@ -8,7 +9,6 @@ Tree.insert(5);
 Tree.insert(17);
 Tree.insert(8);
 Tree.insert(24);
-
 // console.log('tree', Tree);
 
 // DFS
@@ -23,24 +23,30 @@ Tree.insert(24);
 // preorder
 // recursive approach
 function preorder(node) {
-  if (node) {
-    console.log(node.value);
-    preorder(node.left);
-    preorder(node.right);
-  }
-}
-function inorder(node) {       
-  node.left && inorder(node.left); //step 1
   console.log(node.value);
-  node.right && inorder(node.right); //step 3
+  if (node.left) preorder(node.left);
+  if (node.right) preorder(node.right);
+  
 }
-// function inorder(node) {
-//   if (node) {
-//     if (node.left) preorder(node.left);
-//     console.log(node.value);
-//     if (node.right) preorder(node.right);
-//   } 
+// function inorder(node) {       
+//   node.left && inorder(node.left); //step 1
+//   console.log(node.value);
+//   node.right && inorder(node.right); //step 3
 // }
+function inorder(node) {
+  if (node) {
+    if (node.left) inorder(node.left);
+    console.log(node.value);
+    if (node.right) inorder(node.right);
+  } 
+}
+function postorder(node) {
+  node.left && postorder(node.left); //step 1
+  node.right && postorder(node.right); //step 3 
+  console.log(node.value);
+}
+
 
 // preorder(Tree.root);
-inorder(Tree.root);
+// inorder(Tree.root);
+postorder(Tree.root);
