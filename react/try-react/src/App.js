10721fa69686de4c940823b45d7459773971b9d1
Sum Xu <v-suxu@expedia.com>
2022-06-01 17:37:41 +0800

    update

--- react/try-react/src/App.js
+++ react/try-react/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dog from "./components/dog";
+import Recursive from "./components/RecursiveRendering"
 
 function App() {
   return (
     <div className="App">
       <h1>React app</h1>
-      <Dog />
+      {/* <Dog /> */}
+      <Recursive />
     </div>
   );
 }
