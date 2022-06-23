93d49da8cb5c4417be1f777e3a5c1cc0b055215c
Sum Xu <suxu@expediagroup.com>
2022-06-23 11:37:22 +0800

    update

--- react/try-react/src/App.js
+++ react/try-react/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Dog from "./components/dog";
 import Recursive from "./components/RecursiveRendering";
 import Form from "./components/form";
+import Input from "./components/forwardRef";
 import React from "react";
 
 function App() {
   const inputRef = React.useRef(null);
+  const [value, setValue] = React.useState("");
 
   console.log("ref", inputRef.current);
 
@@ -15,6 +17,8 @@ function App() {
       {/* <Dog /> */}
       {/* <Recursive /> */}
       <Form />
+      <Input label="my field" ref={inputRef} value={value} />
+      <button onClick={() => inputRef.current.log()}>save!</button>
     </div>
   );
 }
