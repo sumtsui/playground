201c39d5e40b59ef7bff4164a7c3934acb02847f
Sum Xu <suxu@expediagroup.com>
2022-08-03 14:23:06 +0800

    update react-playground

--- react/react-playground/src/components/update-object-in-array/index.js
+++ react/react-playground/src/components/update-object-in-array/index.js
@@ -5,26 +5,28 @@ const data = new Array(10).fill().map((a, idx) => ({ idx }));
 function List() {
   const [list, setList] = React.useState(data);
 
-  function onEdit(idx) {
-    setList((prev) => {
-      // return [
-      //   ...prev.slice(0, itemValue),
-      //   { ...prev[itemValue], value: 10000 },
-      //   ...prev.slice(itemValue + 1),
-      // ];
-      // return prev.map((p) =>
-      //   p.value === itemValue ? { ...p, price: 10000 } : p
-      // );
-      const cur = [...prev];
-      cur[idx] = { ...cur[idx], value: 10000 };
-      return cur;
-    });
-  }
-
   return (
     <div>
       {list.map((item) => (
-        <Item data={item} onEdit={onEdit} key={item.idx} />
+        <Item
+          data={item}
+          onEdit={(idx) => {
+            setList((prev) => {
+              // return [
+              //   ...prev.slice(0, itemValue),
+              //   { ...prev[itemValue], value: 10000 },
+              //   ...prev.slice(itemValue + 1),
+              // ];
+              // return prev.map((p) =>
+              //   p.value === itemValue ? { ...p, price: 10000 } : p
+              // );
+              const cur = [...prev];
+              cur[idx] = { ...cur[idx], value: 10000 };
+              return cur;
+            });
+          }}
+          key={item.idx}
+        />
       ))}
     </div>
   );
