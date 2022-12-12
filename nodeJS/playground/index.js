f77a4e09dfb23669dfef999daa5b3395fdff2263
Haochen <haochen.xu@eventx.io>
2022-12-12 23:09:02 +0800

    update

---
+++ nodeJS/playground/index.js
@@ -0,0 +1,14 @@
+var crypto = require('crypto');
+
+// time=1670558995,sig1=ad3a30232fa788da1add5525748e19a1a13af0c6a9f4ba829e5853a6129ec5b0
+
+// var key = 'b6c42ba713a6f6a16f76fbd968ddb569042196eb';
+// var message = 'string from step 2';
+
+// var hash = crypto.createHmac('sha256', key)
+//   .update(message);
+
+// const encoded = hash.digest('hex');
+
+// console.log('hash', encoded);
+
