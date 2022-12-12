eb3866dae54e802534aa741e6ac27179ca8d531c
Haochen <haochen.xu@eventx.io>
2022-12-12 23:08:52 +0800

    update

--- server/serverExpress.js
+++ server/serverExpress.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// parse application/json
+app.use(bodyParser.json());
 const router = express.Router();
 
 app.use(router);
 
 router.get('/haha', (req, res) => res.send('Hello World! /haha'));
 router.get('/haha/good', (req, res) => res.send('Hello World! /haha/good'));
+router.post('/webhooks/cloudflare-stream/upload-finished', (req, res) => {
+  console.log('req header', req.headers);
+  console.log('req', req);
+  res.send('got it');
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
